1b302586f8572d63d0160779d186a3387f3dbb50
My Name <ankitpatil2341@gmail.com>
2025-06-18 11:03:04 +0530

    portfolio

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/ankit2341.github.io', // replace with your actual repo name
-  assetPrefix: '/ankit2341.github.io/',
+ 
 };
 
-export default nextConfig;
+module.exports = nextConfig;
